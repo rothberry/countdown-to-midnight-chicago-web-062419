384291a232d4e358954adea13a67710f0738bb05
Phil Roth <phil.roth077@gmail.com>
2019-06-04 22:48:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,13 @@ def countdown(n)
   puts "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
 #countdown(10)
